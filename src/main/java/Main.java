a6ce261151ce8c84312c7e5e49e5700703b1b5d4
jtdejesus <157234255+jtdejesus@users.noreply.github.com>
2024-11-06 17:06:57 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    double onemile = 1.609;
+    double twomiles = 2.218;
+    double ninemiles = 14.481;
+    double tenmiles = 16.09;
+    System.out.println("Miles      Kilometers");
+    System.out.println("1          " + onemile);
+    System.out.println("2          " + twomiles);
+    System.out.println("9          " + ninemiles);
+    System.out.println("10         " + tenmiles);
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
